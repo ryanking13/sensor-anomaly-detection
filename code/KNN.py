006f10b29a8af5c8b43485ab7751d24b4d297606
sihadan <def6488@gmail.com>
2017-05-27 21:37:42 +0900

    fix knn

--- code/KNN.py
+++ code/KNN.py
@@ -7,61 +7,93 @@ import fastdtw
 
 class KNN:
 
-    def __init__(self, k=1):
+    def __init__(self, k=1, distance_method='UD', neighbor_method='SIMPLE'):
         self.k = k  # num of neighbors
         self.train_data_set = []    # [ Matrix1( num of sensors * timesteps ), ... ]
         self.train_label_set = []   # [ wafer label1, ... ]
         self.n_sensors = 42
 
+        self.distance_method = distance_method
+        self.neighbor_method = neighbor_method
+
         # for Eros
-        self.train_eigenvectors = np.array([])
-        self.w_eigenvalues = np.array([])
+        self.w_eigenvalues = np.zeros(self.n_sensors)
 
     #################################################
     # TRAIN METHODS (interface)
     #################################################
 
-    # 기존에 학습한 Train data를 로드한다.
+    # Load trained data to model
     # TODO: implement
     def load_train_data(self):
         return
 
-    # Train data를 반환한다.
+    # return train data
     def get_train_data(self):
         return self.train_data_set, self.train_label_set
 
-    # 새로운 train data를 추가한다.
+    # add new train data
     def add_train_data(self, train_data, train_label):
-        self.train_data_set.append(train_data)
+
+        method = self.distance_method
+
+        if method == "UD":
+            self.train_data_set.append(train_data)
+
+        elif method == "Eros":
+            self.train_data_set.append(self.eros_setup(train_data))
+
+        elif method == "DTW":
+            self.train_data_set.append(train_data)
+
+        elif method == "FastDTW":
+            self.train_data_set.append(train_data)
+
         self.train_label_set.append(train_label)
 
+    # Eros를 사용하기 위한 초기작업을 수행한다
+    # 각 train data의 eigenvalue 값을 구한다
+    # train data의 eigenvalue 값을 이용하여 곱해질 w를 구한다
+    def eros_setup(self, data):
+
+        V, s = self.get_svd(data)
+
+        for i in range(self.n_sensors):
+            self.w_eigenvalues[i] = max(self.w_eigenvalues[i], s[i])
+
+        return V
+
+    # 행렬의 svd 분석에서 나오는 eigenvalue 값을 구한다
+    def get_svd(self, mat):
+        U, s, V = np.linalg.svd(mat)
+
+        sum_s = np.sum(s)
+        s = np.divide(s, sum_s)
+
+        return V, s
 
     #################################################
     # TEST METHODS
     #################################################
 
-    # Test data(웨이퍼)의 양불을 추정하고 정확도를 확인한다.
-    def test(self, test_data_set, test_label_set, distance_method='UD', neighbor_method = 'SIMPLE'):
+    def test(self, test_data_set, test_label_set):
 
-        predicted = self.predict(test_data_set, distance_method=distance_method, neighbor_method=neighbor_method)
+        predicted = self.predict(test_data_set)
         test_label_set = np.reshape(np.array(test_label_set), -1)
         accuracy = 1 - (predicted != test_label_set).sum() / float(predicted.size)
         return test_label_set, predicted, accuracy
 
-    # Test data(웨이퍼)의 양불을 추정한다.
-    def predict(self, test_data_set, distance_method='UD', neighbor_method = 'SIMPLE'):
-
-        if distance_method == 'Eros':
-            self.eros_setup()
+    def predict(self, test_data_set):
 
         train_data_set, train_label_set = self.get_train_data()
 
         predicted = []
         for test_data in test_data_set:
-            if neighbor_method == 'SIMPLE':
-                neighbors = self.get_neighbors(train_data_set, train_label_set, test_data, method=distance_method)
-            elif neighbor_method == 'BORDA':
+            if self.neighbor_method == 'SIMPLE':
+                neighbors = self.get_neighbors(train_data_set, train_label_set, test_data)
+            elif self.neighbor_method == 'BORDA':
                 neighbors = self.get_neighbors_BORDA(train_data_set, train_label_set, test_data)
+
             predicted.append(np.bincount(neighbors).argmax())
 
         return np.array(predicted)
@@ -71,9 +103,8 @@ class KNN:
     # Distance methods
     #################################################
 
-    # Uclidean distance를 구한다.
-    # 두 Matrix의 크기가 같음을 가정
-    # 다를 경우 수정이 필요함
+    # get Uclidean distance
+    # assume size of two matrix is same ( if not, error )
     def get_uclidean_distance(self, mat1, mat2, per_column=False):
 
         diff = np.absolute(mat1-mat2)
@@ -85,15 +116,6 @@ class KNN:
 
         return dist
 
-    # 행렬의 svd 분석에서 나오는 eigenvalue 값을 구한다
-    def get_eigenvalues(self, mat):
-        U, s, V = np.linalg.svd(mat)
-
-        sum_s = np.sum(s)
-        s = np.divide(s, sum_s)
-
-        return V, s
-
     # Eros(Extended Frobenius norm)을 이용한 distance를 계산한다
     # V1, V2 = right eigenvector matrix
     def get_eros_distance(self, V1, V2):
@@ -104,39 +126,9 @@ class KNN:
 
         return eros
 
-    # Eros를 사용하기 위한 초기작업을 수행한다
-    # 각 train data의 eigenvalue 값을 구한다
-    # train data의 eigenvalue 값을 이용하여 곱해질 w를 구한다
-    def eros_setup(self, w_method='average'):
-        print("[*] Staring Eros setup...")
-        data, _ = self.get_train_data()
-
-        # 각 Train data에 대한 eigenvalue를 구한다
-        V_rows = []
-        s_rows = []
-        for d in data:
-            V, s = self.get_eigenvalues(d)
-            V_rows.append(V)
-            s_rows.append(s)
-
-        self.train_eigenvectors = np.array(V_rows)
-
-        # 곱해질 w를 구한다
-        # TODO : reimplement w_
-        if w_method == 'max':
-            self.w_eigenvalues = np.max(np.array(s_rows), axis=0)
-        elif w_method == 'min':
-            self.w_eigenvalues = np.min(np.array(s_rows), axis=0)
-        elif w_method == 'average':
-            self.w_eigenvalues = np.average(np.array(s_rows), axis=0)
-
-        print("[*] Ended Eros setup")
-        # self.w_eigenvalues = np.ones(self.w_eigenvalues.shape) # TEMP
-        return self.w_eigenvalues
-
-    # Dynamic Time Warping을 사용한 두 Matrix의 distance를 계산한다
-    # 두 Matrix의 크기가 같음을 가정
-    # -- 심각하게 느림 --
+    # get distance by Dynamic Time Warping method
+    # assume size of two matrix is same ( if not, error )
+    # very slow
     def get_dtw(self, mat1, mat2):
 
         global_cost_sum = 0
@@ -169,8 +161,8 @@ class KNN:
 
         return global_cost_sum
 
-    # Fast Dynamic Time Warping을 사용한 두 Matrix의 distance를 계산한다
-    # -- 느림 --
+    # get distance by Fase Dynamic Time Warping method
+    # slow
     def get_fast_dtw(self, mat1, mat2):
 
         global_cost_sum = 0
@@ -231,13 +223,15 @@ class KNN:
 
     # 각 Train data에 대해 Test data와의 거리를 구한다
     # 가장 가까운 k개의 data(neighbor)의 label을 반환한다
-    def get_neighbors(self, train_data_set, train_label_set, test_data, method='UD'):
+    def get_neighbors(self, train_data_set, train_label_set, test_data):
+
         distances = []
+        method = self.distance_method
 
         V2 = None
         if method == 'Eros':
             # Test data의 eigenvalue를 구한다
-            V2, _ = self.get_eigenvalues(test_data)
+            V2, _ = self.get_svd(test_data)
 
         # 각 Train data에 대하여 distance를 구한다
         for i in range(len(train_data_set)):
@@ -246,8 +240,7 @@ class KNN:
             if method == 'UD':
                 dist = self.get_uclidean_distance(np.array(train_data_set[i]), np.array(test_data))
             elif method == 'Eros':
-                V1 = self.train_eigenvectors[i]
-                dist = self.get_eros_distance(V1, V2)
+                dist = self.get_eros_distance(train_data_set[i], V2)
             elif method == 'DTW':
                 dist = self.get_dtw(np.array(train_data_set[i]), np.array(test_data))
             elif method == 'FastDTW':
